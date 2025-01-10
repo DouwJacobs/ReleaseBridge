9644649ca565467cb005a7d2153b1c4a564daac1
Douw Jacobs <douw1.jacobs@gmail.com>
2025-01-10 11:18:07 +0200

    ci(husky-prettier-cz): setup husky, prettier and commitizen for ci cd

--- src/app.js
+++ src/app.js
@@ -8,39 +8,44 @@ const REPO_OWNER = process.env.REPO_OWNER;
 const REPO_NAME = process.env.REPO_NAME;
 
 const githubApi = axios.create({
-    baseURL: 'https://api.github.com',
-    headers: {
-        Authorization: `Bearer ${GITHUB_TOKEN}`,
-    },
+  baseURL: 'https://api.github.com',
+  headers: {
+    Authorization: `Bearer ${GITHUB_TOKEN}`,
+  },
 });
 
 // Endpoint to fetch releases
 app.get('/releases', async (req, res) => {
-    try {
-        const response = await githubApi.get(`/repos/${REPO_OWNER}/${REPO_NAME}/releases`);
-        res.json(response.data);
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).json({ error: 'Failed to fetch release notes' });
-    }
+  try {
+    const response = await githubApi.get(
+      `/repos/${REPO_OWNER}/${REPO_NAME}/releases`
+    );
+    res.json(response.data);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: 'Failed to fetch release notes' });
+  }
 });
 
 // Endpoint to fetch commits
 app.get('/commits', async (req, res) => {
-    const { branch, per_page } = req.query;
+  const { branch, per_page } = req.query;
 
-    try {
-        const response = await githubApi.get(`/repos/${REPO_OWNER}/${REPO_NAME}/commits`, {
-            params: {
-                branch,
-                per_page
-            },
-        });
-        res.json(response.data);
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).json({ error: 'Failed to fetch commits' });
-    }
+  try {
+    const response = await githubApi.get(
+      `/repos/${REPO_OWNER}/${REPO_NAME}/commits`,
+      {
+        params: {
+          branch,
+          per_page,
+        },
+      }
+    );
+    res.json(response.data);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: 'Failed to fetch commits' });
+  }
 });
 
 // Start the server
